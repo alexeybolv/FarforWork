071dcf1f143b9b4bc9148d66d72d7fc74f41778b
Алексей <alexey@macbook-pro-aleksej.local>
2016-05-16 00:22:49 +0300

    Initial Commit

---
+++ FarforWork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FarforWork
+//
+//  Created by Алексей on 16.05.16.
+//  Copyright © 2016 Alexey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
